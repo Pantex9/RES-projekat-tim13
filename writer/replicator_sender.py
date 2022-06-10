6fcb227d563eecd581798a4b7eba9e879c81dd6a
Igor Jelic <igor.jelic.98@gmail.com>
2022-06-10 15:34:28 +0200

    Spajanje mog i Jeleninog koda modula replicator_sender

--- writer/replicator_sender.py
+++ writer/replicator_sender.py
@@ -1,10 +1,18 @@
+from Reciever.Receiver import Send, ReplicatorReceiver
 from assets.helper import ReceiverProperty, CODE
+from multiprocessing.connection import wait
+import queue
+import threading
+from time import sleep
+from tracemalloc import start
 
 
 class ReplicatorSender:
     def __init__(self):
         self.thread = None
         self.queue = []
+        self.replicatorReceiver = ReplicatorReceiver()
+        threading.Thread = start()
 
     def repl_sender_receive(self, code, value):
         # potrebna je struktura koja bi sacuvala vrednosti CODE i VALUE
@@ -14,3 +22,15 @@ class ReplicatorSender:
         print("Trenutno stanje QUEUE-a:")
         for rp in self.queue:
             print(f"Code[{CODE(rp.code)}] = {rp.value}")
+
+    def repl_sender_send(self):
+        receiverProp = queue[ReceiverProperty]
+        mutex = threading.Lock()
+        while (True):
+            while (receiverProp <= 0):
+                self = wait()
+
+            rp = receiverProp
+            self.queue.pop(rp)
+            self.replicatorReceiver = Send(CODE(rp.code), rp.value)
+            threading.Thread = sleep(1000)
