6e570b1defcfdf43cca0280ec02143c63af8bee5
djokicd1811 <101884362+djokicd1811@users.noreply.github.com>
2022-06-20 01:56:15 +0200

    modifikovan logger tj upisivanje u .txt fajl

--- Reciever/logger.py
+++ Reciever/logger.py
@@ -1,41 +1,27 @@
 
-
 from codecs import StreamWriter
-from dataclasses import dataclass
 import string
 import threading
-from xmlrpc.client import DateTime
+from datetime import datetime
+from assets.helper import CODE
 
 
 class Logger:
-    
-    sw=StreamWriter()
-    filename=string
-    dateTime=DateTime()
+    def __init__(self, fileName):
+        self.fileName = fileName
 
-    def __init__(self,fileName):
-        self.filename=fileName
-        #mutex=Mutex()
+    def LoggSentCodes(self, code: CODE, value: int, dateTime: datetime, writerId: int):
+        threading.Lock()
+        f = open(self.fileName, "a")
+        f.write(f"\n{dateTime} Writer id: {writerId} sent:   {code} Value:  {value}")
+        f.close()
 
-    def LoggSentCodes(self,code,value,dateTime,writerId):
-        my_mutex=threading.Lock()
-        with StreamWriter(self._fileName, True) as self._sw:
+    def LoggStoredCodes(self, code=CODE, value=int, dateTime=datetime):
+        f = open(self.fileName, "a")
+        f.write(f"\n{dateTime} DATA STORED: {code}, Value:  {value}")
+        f.close()
 
-                self._sw.writelines("\n{0:s} Writer id: {1:s} sent:   {2:s} Value:  {3:s}".format(dateTime, writerId, code, value))
-                self._sw.Close()
-    
-
-
-
-    def LoggStoredCodes(self, code, value, dateTime):
-        with StreamWriter(self._fileName, True) as self._sw:
-
-            self._sw.writelines("\n{0:s} DATA STORED: {1:s}, Value:  {2:s}".format(dateTime, code, value))
-            self._sw.Close()
-
-
-    def LoggActivity(self, activity):
-        with StreamWriter(self._fileName, True) as self._sw:
-
-            self._sw.writelines("\n"+activity+" "+DateTime.Now)
-            self._sw.Close()
+    def LoggActivity(self, activity: string):
+        f = open(self.fileName, "a")
+        f.write(f"\n {activity} ; {str(datetime.now)}")
+        f.close()
