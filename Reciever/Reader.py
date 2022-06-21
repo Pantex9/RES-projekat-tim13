451de145d6f6d47cbdce091e5eb359e57b12f5f7
djokicd1811 <101884362+djokicd1811@users.noreply.github.com>
2022-06-22 01:56:46 +0200

    refaktorizacija koda klase Reader

--- Reciever/Reader.py
+++ Reciever/Reader.py
@@ -1,9 +1,8 @@
+import os
 from datetime import datetime
-from assets.helper import ReceiverProperty, CODE
-from Reciever.FileItem import FileItem
-from Reciever.logger import Logger
-
-from codecs import StreamReader, StreamWriter
+from assets.helper import ReceiverProperty, CODE, DeltaCD
+from ReplicatorReceiver.FileItem import FileItem
+from ReplicatorReceiver.Logger import Logger
 
 
 class CollectionFileItems:
@@ -12,34 +11,22 @@ class CollectionFileItems:
 
 class Reader:
     def __init__(self, fileName):
+        self.currentVal = 1
         self.fileName = fileName
-        self.l: Logger = Logger(r"C:\Users\Pantex\PycharmProjects\pythonProject\BazaPodataka\PracenjeAktivnosti")
+        self.l: Logger = Logger(r"C:\Users\Pantex\PycharmProjects\pythonProject\BazaPodataka\LOG\readerLogs.txt")
+        self.niz = []
+        self.dateTime = datetime.now().strftime("%d-%m-%y %H:%M:%S")
 
-    def WriteInFile(self, receiverProperty: ReceiverProperty):
-        if receiverProperty.Code == CODE.CODE_DIGITAL:
-            with StreamWriter(self.fileName, str(True)) as self.sw:
-                self.sw.writelines(
-                    str(datetime.now) + ";" + str(receiverProperty.Code) + ";" + str(receiverProperty.ReceiverValue))
-                self.l.LoggStoredCodes(receiverProperty.Code, receiverProperty.ReceiverValue, datetime.now)
-                return
-
-        with StreamReader(self.fileName) as self.sr:
-            while True:
-                st = self.sr.readline()
-                if st == "<EOF>" or st is None:
-                    break
-                else:
-                    niz = st.split(";")
-                    pom1 = datetime.strptime(niz[0], "%d/%m/%y %H:%M:%S.%f")
-                    pom2 = CODE[niz[1]]
-                    pom3 = int(niz[2])
-                    it: FileItem = FileItem(dateTime=pom1, rp=ReceiverProperty(code=pom2, receiverValue=pom3))
-                    if it.rp.Code == receiverProperty.Code:
-                        difference = abs(it.rp.ReceiverValue - receiverProperty.ReceiverValue)
-                        if difference < it.rp.ReceiverValue * 0.02:
-                            return
-
-        with StreamWriter(self.fileName, str(True)) as self.sw:
-            self.sw.writelines(
-                str(datetime.now) + ";" + str(receiverProperty.Code) + ";" + str(receiverProperty.ReceiverValue))
-            self.l.LoggStoredCodes(receiverProperty.Code, receiverProperty.ReceiverValue, datetime.now)
+    def write_in_file2(self, delta_cd: DeltaCD):
+        for item in delta_cd.update:
+            # Updatuj trenutnu vrednost i loguj ako treba?
+            new_value = item.ReceiverValue
+            self.currentVal = new_value
+        for item in delta_cd.add:
+            # Upisi u bazu podataka i loguj ako treba?
+            new_rp = item
+            f = open(self.fileName, "a")
+            f.write(f"\n{self.dateTime}  ;  {new_rp.Code}  ; {new_rp.ReceiverValue}")
+            self.l.LoggStoredCodes(new_rp.Code, new_rp.ReceiverValue, self.dateTime)
+            self.currentVal = new_rp.ReceiverValue
+            f.close()
